9194b95fecdb02ad7b14cfcb7dc0ed8f5f8f6bea
Roushan Kumar <roushan.astrazeneca@google.com>
2026-05-11 18:33:45 +0530

    feat: update Dashboard and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Whenever React asks for /api, Vite secretly forwards it to FastAPI
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000', // 👈 CHANGE THIS FROM localhost
         changeOrigin: true,
         secure: false,
       }
